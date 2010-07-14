b252d0bd7728ce921772473a7fcb928f4a002ef9
Sam Mackrill <Sam Mackrill@localhost>
2010-07-14 17:50:24 +0100

    upgrade to lesson07 ++smudge

--- src/com/mackrill/mackcube/Run.java
+++ src/com/mackrill/mackcube/Run.java
@@ -1,52 +1,49 @@
 package com.mackrill.mackcube;
 
 import android.app.Activity;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 /**
  * The initial Android Activity, setting and initiating
- * the OpenGL ES Renderer Class @see Lesson06.java
+ * the OpenGL ES Renderer Class @see Lesson07.java
  * 
  * @author Savas Ziplies (nea/INsanityDesign)
  */
 public class Run extends Activity {
 
-	/** The OpenGL View */
-	private GLSurfaceView glSurface;
+	/** Our own OpenGL View overridden */
+	private MackCube mackCube;
 
 	/**
-	 * Initiate the OpenGL View and set our own
-	 * Renderer (@see Lesson06.java)
+	 * Initiate our @see Lesson07.java,
+	 * which is GLSurfaceView and Renderer
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		//Create an Instance with this Activity
-		glSurface = new GLSurfaceView(this);
-		//Set our own Renderer and hand the renderer this Activity Context
-		glSurface.setRenderer(new MackCube(this));
-		//Set the GLSurface as View to this Activity
-		setContentView(glSurface);
+		
+		//Initiate our Lesson with this Activity Context handed over
+		mackCube = new MackCube(this);
+		//Set the lesson as View to the Activity
+		setContentView(mackCube);
 	}
 
 	/**
-	 * Remember to resume the glSurface
+	 * Remember to resume our Lesson
 	 */
 	@Override
 	protected void onResume() {
 		super.onResume();
-		glSurface.onResume();
+		mackCube.onResume();
 	}
 
 	/**
-	 * Also pause the glSurface
+	 * Also pause our Lesson
 	 */
 	@Override
 	protected void onPause() {
 		super.onPause();
-		glSurface.onPause();
+		mackCube.onPause();
 	}
-	
+
 }
